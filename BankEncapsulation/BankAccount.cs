b1322edd52bf2d7b4e888d2785c4c81a6ffa077d
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-30 13:37:06 -0500

    bank done

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double money) 
+        {
+            balance = money;        
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine($"The balance in your account is ${ balance }");
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Silent Hill Bank");
+            
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("How much money do you want to deposit today?:$");
+            var userinput = double.Parse(Console.ReadLine());
+            account.Deposit(userinput);
+            account.GetBalance();
+
+
         }
     }
 }
